4d56582d57c21e051020131e2deb47a7fc51b5d5
fluwins <fluwins@users.noreply.github.com>
2019-07-19 23:41:01 +0700

    Update demo module system

---
+++ demo_module_system/com.hello.new/hello.service/com/hello/service/PersonService.java
@@ -0,0 +1,18 @@
+package com.hello.service;
+
+import com.hello.model.Person;
+
+public class PersonService {
+
+	public Person getPerson() {
+		return new Person();
+	}
+	
+	public void addPerson() {
+		//Add new person
+	}
+	
+	public void deletePerson() {
+		//Delete person
+	}
+}
